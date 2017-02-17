3ecebacf2313b2f25698a13fc7abc91e88c6cea0
AlexAegis <alexaegis@gmail.com>
2017-02-17 21:30:32 +0100

    clipboardreader added

--- toolbox/src/main/java/com/github/alexaegis/clipboard/ClipBoardReader.java
+++ toolbox/src/main/java/com/github/alexaegis/clipboard/ClipBoardReader.java
@@ -2,16 +2,13 @@ package com.github.alexaegis.clipboard;
 
 import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
-
 
 public class ClipBoardReader {
     public static String readFromClipBoard() {
         String result = "";
         try {
             result = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
-        } catch (UnsupportedFlavorException | IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
