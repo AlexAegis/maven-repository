4e4d3bcc5e8d35c42f55d5822ec083c5ef2b6294
AlexAegis <alexaegis@gmail.com>
2017-02-17 18:07:49 +0100

    working pom

--- swing-boardgame/src/main/java/com/alexaegis/archetypes/App.java
+++ swing-boardgame/src/main/java/com/alexaegis/archetypes/App.java
@@ -2,6 +2,6 @@ package com.alexaegis.archetypes;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Repo!");
+        System.out.println("Hello Repo, pom done!");
     }
 }
